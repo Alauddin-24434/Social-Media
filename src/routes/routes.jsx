22e3d226c8f18c3a1435ff50eeb360bc5fa6446e
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2024-05-04 17:04:10 +0800

    upadate code

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
+import Mainlayout from "../layouts/Mainlayout";
+import Home from "../pages/Home/Home";
+import Error from "../pages/Error/Error";
+import Signup from "../pages/Signup/Signup";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Mainlayout/>,
+      errorElement:<Error/>,
+      children:[
+        {
+            path:'/',
+            element:<Home/>
+        },
+      
+      ]
+    },
+    {
+        path:'/signup',
+        element:<Signup/>
+    },
+    {
+        path:'/login',
+        element:<Login/>
     },
   ]);
 
